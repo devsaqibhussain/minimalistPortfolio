d30c30b44cdd42da8398ef1db1182ae680ae1f7a
Saqib Hussain <dev.saqibhussain@gmail.com>
2023-11-03 11:16:47 +0500

    Fix: Nav bar background color mismatch on firefox

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import { ModeToggle } from "./ModeToggle";
-import  NavMenuToggle  from "./NavMenuToggle"
+import NavMenuToggle from "./NavMenuToggle";
 import Link from "next/link";
 
 const NavBar = () => {
-  
   return (
     <div>
-      <div className=" flex flex-row justify-between items-center fixed top-0 right-0 left-0 py-2 container bg-white dark:bg-[#121212]">
+      <div className=" flex flex-row justify-between items-center fixed top-0 right-0 left-0 py-2 container bg-white dark:bg-[#0c0a09] shadow-md dark:shadow-lg rounded-full">
         <div className="text-xl font-semibold ">
           <Link href={"/"}>Dev Saqib</Link>
         </div>
         <div className="flex items-center gap-2 md:gap-4">
           <ModeToggle />
-          <NavMenuToggle/>
+          <NavMenuToggle />
         </div>
       </div>
     </div>
